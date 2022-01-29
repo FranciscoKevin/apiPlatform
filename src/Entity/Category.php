fb259212633a1aea1614412ed0b47d4fbad9a286
FrKev1 <francisco.kevin37@gmail.com>
2022-01-29 16:22:58 +0100

    The serialization

---
+++ src/Entity/Category.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    #[Groups(["read:Post"])]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(["read:Post"])]
+    private $name;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Post::class)]
+    private $posts;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Post[]
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): self
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts[] = $post;
+            $post->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): self
+    {
+        if ($this->posts->removeElement($post)) {
+            // set the owning side to null (unless already changed)
+            if ($post->getCategory() === $this) {
+                $post->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -4,31 +4,58 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ["groups" => ["read:collection"]],
+    denormalizationContext: ["groups" => ["write:Post"]],
+    itemOperations: [
+        "put",
+        "delete",
+        "get" => [
+            "normalization_context" => ["groups" => ["read:collection", "read:item", "read:Post"]]
+        ]
+    ]
+)]
 class Post
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(["read:collection"])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(["read:collection", "write:Post"])]
     private $title;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(["read:collection", "write:Post"])]
     private $slug;
 
     #[ORM\Column(type: 'text')]
+    #[Groups(["read:item", "write:Post"])]
     private $content;
 
     #[ORM\Column(type: 'datetime')]
+    #[Groups(["read:item"])]
     private $createdAt;
 
     #[ORM\Column(type: 'datetime')]
     private $updatedAt;
+
+    #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'posts')]
+    #[Groups(["read:item", "write:Post"])]
+    private $category;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTime();
+        $this->updatedAt = new DateTime();
+    }
 
     public function getId(): ?int
     {
@@ -91,6 +118,18 @@ class Post
     public function setUpdatedAt(\DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
